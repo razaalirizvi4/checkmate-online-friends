c3b903b1a9c62cb6956571cb8332372da8f76309
mbargit <trickerzyt@gmail.com>
2025-06-21 23:37:00 +0100

    change login

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import { Buffer } from "buffer";
 
 window.Buffer = Buffer;
 
+document.documentElement.classList.add('dark');
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
